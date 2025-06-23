e998c803da121e6d799a20fe994977a3c44a6e65
Wadiie <wadie.gaddour@esprit.tn>
2025-06-23 13:09:36 +0200

    commit deploy

--- src/components/experienceSection/ExperienceTopMiddle.jsx
+++ src/components/experienceSection/ExperienceTopMiddle.jsx
@@ -1,8 +1,9 @@
-import experienceimage from '../../assets/subSkills.jpg';
+import subSkills from '../../assets/subSkills.jpg';
+import SubSkills from '../skillsSection/SubSkills';
 const ExperienceTopMiddle = () => {
   return (
     <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
-      <img src={experienceimage} alt="my experience" />
+      <img src={SubSkills} alt="my experience" />
     </div>
   );
 };
